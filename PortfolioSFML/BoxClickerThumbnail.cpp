0d240a72057ba45ae4eb7edda47ce6f047139d63
Michael Challinor <92083194+c026128l@users.noreply.github.com>
2023-08-19 21:09:36 +0100

    Implemented thumbnail class
    - Setup implementation in Games Menu
    - Created a vector in games menu for handling renders/updates etc on all thumbnails.

--- PortfolioSFML/BoxClickerThumbnail.cpp
+++
@@ -1,9 +0,0 @@
-#include "BoxClickerThumbnail.h"
-
-void BoxClickerThumbnail::initTexture()
-{
-	if (!this->spriteTexture->loadFromFile("Resources/Textures/boxClickerThumbnail.png"))
-	{
-		std::cout << "ERROR::GAMESMENU::INITBUTTONS:: Failed to load boxClickerThumbnail.png" << std::endl;
-	}
-}
--- PortfolioSFML/GamesMenu.cpp
+++ PortfolioSFML/GamesMenu.cpp
@@ -8,6 +8,7 @@ void GamesMenu::initVar(sf::VideoMode screen_bounds)
 	this->mouseHeld = false;
 	this->screenBounds = screen_bounds;
 	this->boxClickerLaunched = false;
+
 }
 
 void GamesMenu::initBackground()
@@ -48,7 +49,7 @@ void GamesMenu::initText()
 	this->boxClickerTitleText.setCharacterSize(32);
 	this->boxClickerTitleText.setFillColor(sf::Color::White);
 	this->boxClickerTitleText.setString("Box Clicker");
-	this->boxClickerTitleText.setPosition(this->boxClickerButton.getGlobalBounds().left + this->boxClickerButton.getGlobalBounds().width / 2 - this->boxClickerTitleText.getGlobalBounds().width / 2, this->boxClickerButton.getGlobalBounds().top - this->boxClickerTitleText.getGlobalBounds().height * 1.5);
+	this->boxClickerTitleText.setPosition(this->boxClickerThumbnail->getHighlightBox().getGlobalBounds().left + this->boxClickerThumbnail->getHighlightBox().getGlobalBounds().width / 2 - this->boxClickerTitleText.getGlobalBounds().width / 2, this->boxClickerThumbnail->getHighlightBox().getGlobalBounds().top - this->boxClickerTitleText.getGlobalBounds().height * 1.5);
 }
 
 void GamesMenu::initButtons()
@@ -57,27 +58,20 @@ void GamesMenu::initButtons()
 	this->buttonColour = sf::Color(177, 177, 177, 177);
 	this->buttonHighlightedColour = sf::Color::White;
 
-	//Set selected/deselected rect for game sprites
-	this->selectedRect = sf::IntRect(0, 0, 800, 600);
-	this->deselectedRect = sf::IntRect(200, 0, 600, 600);
-
-	//Init the BoxClicker button texture
-	if (!this->boxClickerTexture.loadFromFile("Resources/Textures/boxClickerThumbnail.png"))
-	{
-		std::cout << "ERROR::GAMESMENU::INITBUTTONS:: Failed to load boxClickerThumbnail.png" << std::endl;
-	}
+	this->boxClickerThumbnail = new BaseGameThumbnails();
+	this->ballSwagThumbnail = new BaseGameThumbnails();
 
 	//Init Box Clicker button sprite
-	this->boxClickerSprite.setTexture(this->boxClickerTexture);
-	this->boxClickerSprite.setTextureRect(this->deselectedRect);
-	this->boxClickerSprite.setScale(0.35f, 0.35f);
-	this->boxClickerSprite.setPosition(10.f, this->screenBounds.height / 2 - this->boxClickerSprite.getGlobalBounds().height / 2);
-	this->gameSprites.push_back(this->boxClickerSprite);
-
-	//Init BoxClicker button background
-	this->boxClickerButton.setFillColor(this->buttonColour);
-	this->boxClickerButton.setSize(sf::Vector2f(this->boxClickerSprite.getGlobalBounds().width + 10.f, this->boxClickerSprite.getGlobalBounds().height + 10.f));
-	this->boxClickerButton.setPosition(this->boxClickerSprite.getGlobalBounds().left - 5.f, this->boxClickerSprite.getGlobalBounds().top - 5.f);
+	this->boxClickerThumbnail->initTexture("Resources/Textures/boxClickerThumbnail.png");
+	this->boxClickerThumbnail->initSprite();
+	this->boxClickerThumbnail->setPosition(10.f, this->screenBounds.height / 2 - this->boxClickerThumbnail->getSprite().getGlobalBounds().height / 2);
+	this->thumbnails.push_back(this->boxClickerThumbnail);
+	
+	//Init BallSwag button sprite
+	this->ballSwagThumbnail->initTexture("Resources/Textures/ballSwagThumbnail.png");
+	this->ballSwagThumbnail->initSprite();
+	this->ballSwagThumbnail->setPosition(this->boxClickerThumbnail->getHighlightBox().getGlobalBounds().left + this->boxClickerThumbnail->getHighlightBox().getGlobalBounds().width + 10.f, this->screenBounds.height / 2 - this->ballSwagThumbnail->getSprite().getGlobalBounds().height / 2);
+	this->thumbnails.push_back(this->ballSwagThumbnail);
 
 	//Init Back button background
 	this->backButton.setFillColor(this->buttonColour);
@@ -113,11 +107,10 @@ void GamesMenu::menuInteraction(sf::Vector2f mouse_pos)
 
 			this->mouseHeld = true;
 
-			if (this->boxClickerButton.getGlobalBounds().contains(mouse_pos))
+			if (this->boxClickerThumbnail->getHighlightBox().getGlobalBounds().contains(mouse_pos))
 			{
 				this->menuOpen = false;
 				this->boxClickerLaunched = true;
-
 			}
 
 			else if (this->backButton.getGlobalBounds().contains(mouse_pos))
@@ -154,90 +147,57 @@ void GamesMenu::setMenuOpen(bool is_open)
 	this->menuOpen = is_open;
 }
 
-void GamesMenu::updateSpriteSize(sf::Sprite& sprite, sf::IntRect target_size)
-{
-	int temp_width = sprite.getTextureRect().width;
-	int temp_left = sprite.getTextureRect().left;
-
-	if (this->enlargingTimer.getElapsedTime().asMilliseconds() >= 3.75f)
-	{
-		if (sprite.getTextureRect().width < target_size.width || sprite.getTextureRect().left > target_size.left)
-		{
-			if (temp_width != target_size.width)
-			{
-				temp_width += 5;
-				this->enlargingTimer.restart();
-			}
-			if (temp_left != target_size.left)
-			{
-				temp_left -= 5;
-				this->enlargingTimer.restart();
-			}
-		}
-		else if (sprite.getTextureRect().width > target_size.width && sprite.getTextureRect().left < target_size.left)
-		{
-			if (temp_width != target_size.width)
-			{
-				temp_width -= 5;
-			}
-			if (temp_left != target_size.left)
-			{
-				temp_left += 5;
-			}
-		}
-
-		sprite.setTextureRect(sf::IntRect(temp_left, 0, temp_width, 600));
-		this->boxClickerButton.setSize(sf::Vector2f(this->boxClickerSprite.getGlobalBounds().width + 10.f, this->boxClickerSprite.getGlobalBounds().height + 10.f));
-		this->boxClickerTitleText.setPosition(this->boxClickerButton.getGlobalBounds().left + this->boxClickerButton.getGlobalBounds().width / 2 - this->boxClickerTitleText.getGlobalBounds().width / 2, this->boxClickerButton.getGlobalBounds().top - this->boxClickerTitleText.getGlobalBounds().height * 1.5);
-
-		this->enlargingTimer.restart();
-	}
-}
-
-
 //UPDATES
 
 void GamesMenu::update(sf::Vector2f mousePos)
 {
 	this->updateGUI(mousePos);
 
-	if (this->spriteEnlarging)
+	for (size_t i = 1; i < this->thumbnails.size(); i++)
 	{
-		this->updateSpriteSize(this->boxClickerSprite, this->selectedRect);
+		this->thumbnails[i]->setPosition(this->thumbnails[i - 1]->getHighlightBox().getGlobalBounds().left + this->thumbnails[i - 1]->getHighlightBox().getGlobalBounds().width + 10.f, this->thumbnails[i]->getSprite().getGlobalBounds().top);
 	}
-		
-	else if (this->spriteShrinking)
+
+	for (BaseGameThumbnails* thumbnail : this->thumbnails)
 	{
-		this->updateSpriteSize(this->boxClickerSprite, this->deselectedRect);
+		thumbnail->update();
 	}
-		
+
 }
 
 void GamesMenu::updateGUI(sf::Vector2f mousePos)
 {
-	if (this->boxClickerButton.getGlobalBounds().contains(mousePos) && this->boxClickerButton.getFillColor() == this->buttonColour)
-	{
-		this->boxClickerButton.setFillColor(this->buttonHighlightedColour);
 
-		if (!this->spriteEnlarging && this->boxClickerSprite.getTextureRect() != this->selectedRect)
+
+	for (BaseGameThumbnails* thumbnail : this->thumbnails)
+	{
+		if (thumbnail->getSprite().getGlobalBounds().contains(mousePos) && thumbnail->getHighlightColour() == this->buttonColour)
 		{
-			this->spriteShrinking = false;
-			this->spriteEnlarging = true;
+			thumbnail->setHighlightColour(this->buttonHighlightedColour);
+
+			if (!thumbnail->getIsEnlarging())
+			{
+				thumbnail->setIsShrinking(false);
+				thumbnail->setIsEnlarging(true);
+			}
+
+		}
+
+		else if (!thumbnail->getSprite().getGlobalBounds().contains(mousePos) && thumbnail->getHighlightColour() == this->buttonHighlightedColour)
+		{
+			thumbnail->setHighlightColour(this->buttonColour);
+
+			if (!thumbnail->getIsShrinking())
+			{
+				thumbnail->setIsEnlarging(false);
+				thumbnail->setIsShrinking(true);
+			}
+
 		}
 
 	}
 
-	else if (!this->boxClickerButton.getGlobalBounds().contains(mousePos) && this->boxClickerButton.getFillColor() == this->buttonHighlightedColour)
-	{
-		this->boxClickerButton.setFillColor(this->buttonColour);
-
-		if (!this->spriteShrinking && this->boxClickerSprite.getTextureRect() != this->deselectedRect)
-		{
-			this->spriteEnlarging = false;
-			this->spriteShrinking = true;
-		}
-		
-	}
+	
 
 	if (this->backButton.getGlobalBounds().contains(mousePos) && this->backButton.getFillColor() == this->buttonColour)
 	{
@@ -248,6 +208,8 @@ void GamesMenu::updateGUI(sf::Vector2f mousePos)
 	{
 		this->backButton.setFillColor(this->buttonColour);
 	}
+
+
 }
 
 //RENDERS
@@ -283,7 +245,10 @@ void GamesMenu::renderText(sf::RenderTarget& target)
 
 void GamesMenu::renderButtons(sf::RenderTarget& target)
 {
-	target.draw(this->boxClickerButton);
-	target.draw(this->boxClickerSprite);
+	for (BaseGameThumbnails* thumbnail : thumbnails)
+	{
+		thumbnail->render(target);
+	}
+
 	target.draw(this->backButton);
 }
